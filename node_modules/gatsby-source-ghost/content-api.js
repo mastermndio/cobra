e60af57968a051977704206a66253559b98e8c34
Aaron <abrooks@mastermnd.io>
2020-05-30 12:09:00 -0400

    removed node modules

--- node_modules/gatsby-source-ghost/content-api.js
+++
@@ -1,9 +0,0 @@
-const GhostContentAPI = require('@tryghost/content-api');
-
-module.exports.configure = ({apiUrl, contentApiKey, version = `v3`}) => {
-    return new GhostContentAPI({
-        url: apiUrl,
-        key: contentApiKey,
-        version: version
-    });
-};
