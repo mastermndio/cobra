1be41f94d24a5618578fe60fb75e376f64e6fbcc
Aaron <abrooks@mastermnd.io>
2020-05-30 13:12:33 -0400

    randomly tried to break the site

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -11,6 +11,6 @@ module.exports = {
 
     shortTitle: `Ghost`, // Used for App manifest e.g. Mobile Home Screen
     siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
-    backgroundColor: `#e9e9e9`, // Used for Offline Manifest
-    themeColor: `#15171A`, // Used for Offline Manifest
+    backgroundColor: `#1481BA`, // Used for Offline Manifest
+    themeColor: `#000000`, // Used for Offline Manifest
 }
